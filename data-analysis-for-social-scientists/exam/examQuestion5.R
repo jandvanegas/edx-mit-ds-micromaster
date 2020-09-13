dd9ef7a5b5c5c03452466bd3a5bea55b9a0d30d5
hermstefanny <aida.estefania.sosa@gmail.com>
2020-09-13 10:07:20 -0500

    Materials for question 5

---
+++ data-analysis-for-social-scientists/exam/examQuestion5.R
@@ -0,0 +1,12 @@
+#Exercise5
+
+library(perm)
+library(modelr)
+library(np)
+library(tidyverse)
+rm(list = ls())
+
+setwd("/Users/estefaniasosa/Documents/notes/edx-mit-ds-micromaster/data-analysis-for-social-scientists/exam")
+getwd()
+
+houseAndCrime <- read.csv("data_House_Prices_and_Crime_1.csv")
